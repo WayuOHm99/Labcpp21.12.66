4651f6035bbeacad16f5b8d2556206c5f943decb
WayuOHm99 <121616763+WayuOHm99@users.noreply.github.com>
2023-12-21 15:46:29 +0700

    Update range6.cpp

--- LAB21.12.66Cpp/range6.cpp
+++ LAB21.12.66Cpp/range6.cpp
@@ -1,32 +1,33 @@
 /* LANG : C++ */
 #include <iostream>
-#include <limits>
-#include <string>
-
-using namespace std;
 
 int main() {
-    int n,i, max=-1, min=20;
-    for ( i = 1; i < 10; i++) {
-        do {
-            cin >> n;
-            if (n==0)
-            {
-            cout << max - min;
-            exit(0);
+    int n, max = -1, min = 21;
+
+    for (int i = 0; i < 10; ++i) {
+        std::cin >> n;
+
+        if (n == 0) {
+            break;
+        }
+
+        if (n >= 10 && n <= 20) {
+            if (n > max) {
+                max = n;
             }
 
-        } while (n<10 || n>20);
-        if (max<n)
-        {
-        max=n;
-        }
-        if (min>n)
-        {
-        min=n;
+            if (n < min) {
+                min = n;
+            }
+        } else {
+            --i;
         }
     }
-    cout << max - min;
-    
+
+    if (max != -1 && min != 21) {
+        int range = max - min;
+        std::cout << range << std::endl;
+    }
+
     return 0;
 }
